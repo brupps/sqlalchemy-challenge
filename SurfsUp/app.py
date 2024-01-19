4de1cb6db852df9d77c38945ef3e91f6f216b95a
brupps <barbrupps@gmail.com>
2024-01-18 18:09:26 -0600

    Final code changes

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -12,7 +12,7 @@ import datetime as dt
 
 #################################################
 # Database Setup
-#################################################
+#################################################pyt
 engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 
@@ -43,12 +43,22 @@ def welcome():
     """List all available api routes."""
     return (
         f"Welcome to my Hawaii Weather API!<br/>"
+        f"<br/>"
         f"Available Routes:<br/>"
+        f"For rainfall amounts from 2016-08 to 2017-08:<br/>"
         f"/api/v1.0/precipitation<br/>"
+        f"<br/>"
+        f"For a list of weather stations:<br/>"
         f"/api/v1.0/stations<br/>"
+        f"<br/>"
+        f"For a list of temperatures from our most active weather station:<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/start<br/>"
-        f"/api/v1.0/start/end<br/>"
+        f"<br/>"
+        f"For a list of  avg, max, min temps starting from August 2016 in format yyyy-mm-dd:<br/>"
+        f"/api/v1.0/[start_date format:yyyy-mm-dd]<br/>"
+        f"<br/>"
+        f"For a list of avg, max, min temps starting from August 2016 to August 2017 in format yyyy-mm-dd/yyyy-mm-dd:<br/>"
+        f"/api/v1.0/[start_date format:yyyy-mm-dd]/[end_date format:yyyy-mm-dd]<br/>"
     )
 
 # Create first route for precipitation
@@ -106,7 +116,7 @@ def tobs():
     session = Session(engine)
 
     # List the tobs results
-    results = session.query(Measurement.date,  Measurement.tobs,Measurement.prcp).\
+    active_tob = session.query(Measurement.date,  Measurement.tobs,Measurement.prcp).\
         filter(Measurement.date >= '2016-08-23').\
         filter(Measurement.station=='USC00519281').\
         order_by(Measurement.date).all()
@@ -115,7 +125,7 @@ def tobs():
    
     # Convert list of tuples into normal list
     all_tobs = []
-    for prcp, date,tobs in results:
+    for prcp, date,tobs in active_tob :
         tobs_dict = {}
         tobs_dict["prcp"] = prcp
         tobs_dict["date"] = date
@@ -125,9 +135,56 @@ def tobs():
     return jsonify(all_tobs)
 
 
+#Create the fourth route for tobs using the start date
+@app.route("/api/v1.0/<start_date>")
+def Start_date(start_date):
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of min, avg and max tobs for a start date"""
+    # Query tobs for the start date
+    starting = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start_date).all()
+
+    session.close()
+
+    # Create a dictionary using the start date from the tobs
+    start_date_tobs = []
+    for min, avg, max in starting:
+        start_date_tobs_dict = {}
+        start_date_tobs_dict["min_temp"] = min
+        start_date_tobs_dict["avg_temp"] = avg
+        start_date_tobs_dict["max_temp"] = max
+        start_date_tobs.append(start_date_tobs_dict) 
+
+    return jsonify(start_date_tobs)
 
 
+#Create the fifth route for tobs using the start and end dates
+@app.route("/api/v1.0/<start_date>/<end_date>")
+def Start_end_date(start_date, end_date):
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of avg, max, and min tobs for start and end dates"""
+    # Query all tobs for date range
+    start_end = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start_date).filter(Measurement.date <= end_date).all()
+
+    session.close()
+  
+    # Create a dictionary using the start date and end date and append results
+    start_end_tobs = []
+    for min, avg, max in start_end:
+        start_end_tobs_dict = {}
+        start_end_tobs_dict["min_temp"] = min
+        start_end_tobs_dict["avg_temp"] = avg
+        start_end_tobs_dict["max_temp"] = max
+        start_end_tobs.append(start_end_tobs_dict) 
+    
+    return jsonify(start_end_tobs)
 
 
+#Run app
 if __name__ == '__main__':
     app.run(debug=True)
